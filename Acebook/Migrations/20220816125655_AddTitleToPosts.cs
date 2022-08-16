21ebd7bfb2c8e84b13e4c4f916505694c648799f
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-08-16 13:58:53 +0100

    initialized new migration files AddTitleToPosts

---
+++ Acebook/Migrations/20220816125655_AddTitleToPosts.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace acebook.Migrations
+{
+    public partial class AddTitleToPosts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
