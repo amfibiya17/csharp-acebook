da4fe48952b6b15674e44510321f6431abac39ff
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-08-17 10:41:02 +0100

    time next to each post implemented to post model + new migration files added for this feature

---
+++ Acebook/Migrations/20220816174418_AddTimeToPosts.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace acebook.Migrations
+{
+    public partial class AddTimeToPosts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "TimePosted",
+                table: "Posts",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "now()"
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimePosted",
+                table: "Posts");
+        }
+    }
+}
--- Acebook/Models/Post.cs
+++ Acebook/Models/Post.cs
@@ -1,10 +1,15 @@
-namespace acebook.Models;
 using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace acebook.Models;
 
 public class Post
 {
   [Key]
   public int Id {get; set;}
+  // public DateTimeOffset TimePosted {get; set;}
+  public DateTime TimePosted { get; set; } = DateTime.UtcNow;
+
   public string? Title {get; set;}
   public string? Content {get; set;}
   public int UserId {get; set;}
